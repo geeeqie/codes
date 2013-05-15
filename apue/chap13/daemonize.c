5ca69cee7eb60b923ce19d0aa45d5ac51bb76123
Geeeqie <geeeqie@gmail.com>
2013-05-16 00:50:15 +0800

    add chapter 13 for APUE

---
+++ apue/chap13/daemonize.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/resource.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+void daemonize()
+{
+    umask(0);
+    struct rlimit rl;
+    getrlimit(RLIMIT_NOFILE, &rl);
+    int pid;
+    pid = fork();
+    if (pid != 0) {
+        exit(0);
+    }
+    setsid();
+
+    struct sigaction sa;
+    sa.sa_handler = SIG_IGN;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGHUP, &sa, NULL);
+    pid = fork();
+    if (pid != 0)
+        exit(0);
+    chdir("/");
+    if (rl.rlim_max == RLIM_INFINITY)
+        rl.rlim_max = 1024;
+    for (int i = 0; i < rl.rlim_max; i++)
+        close(i);
+
+    int fd0 = open("/dev/null", O_RDWR);
+    int fd1 = dup(0);
+    int fd2 = dup(0);
+}
+
+int main(void)
+{
+    daemonize();
+    while(1) {
+        printf("I'm daemon\n");
+        sleep(1);
+    }
+    return 0;
+}
